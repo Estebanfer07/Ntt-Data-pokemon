d4f46ec06b466507bcc24e90229ab2cf6263414b
Esteban <esteban.fernando07@gmail.com>
2022-03-06 18:58:38 -0500

    form tests

--- src/app/components/create-edit-form/create-edit-form.component.spec.ts
+++ src/app/components/create-edit-form/create-edit-form.component.spec.ts
@@ -1,25 +1,170 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  TestBed,
+  ComponentFixtureAutoDetect,
+} from '@angular/core/testing';
 
 import { CreateEditFormComponent } from './create-edit-form.component';
+import { Pokemon } from '../../models/pokemon';
+import { StoreModule } from '@ngrx/store';
+import { appReducers, AppState } from '../../store/app.reducer';
+import { HttpClientModule } from '@angular/common/http';
+import { PokemonService } from '../../services/pokemon.service';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SliderComponent } from '../slider/slider.component';
+import { By } from '@angular/platform-browser';
+import { of } from 'rxjs';
 
 describe('CreateEditFormComponent', () => {
   let component: CreateEditFormComponent;
   let fixture: ComponentFixture<CreateEditFormComponent>;
+  let service: PokemonService;
+  let store: MockStore<AppState>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ CreateEditFormComponent ]
-    })
-    .compileComponents();
-  });
+  const pokemons: Pokemon[] = [
+    {
+      id: 645,
+      name: 'Pikachu',
+      image:
+        'https://imagenpng.com/wp-content/uploads/2016/09/Pikachu-png-1-635x624.png',
+      type: 'fire',
+      hp: 77,
+      attack: 100,
+      defense: 100,
+      idAuthor: 1,
+      created_at: new Date('2022-02-24T16:09:23.247Z'),
+      updated_at: new Date('2022-03-05T22:55:29.095Z'),
+    },
+    {
+      id: 668,
+      name: 'LUCARIO editado',
+      image:
+        'https://assets.pokemon.com/assets/cms2/img/pokedex/full/448_f2.png',
+      type: 'poison',
+      hp: 34,
+      attack: 40,
+      defense: 77,
+      idAuthor: 1,
+      created_at: new Date('2022-02-28T15:56:24.375Z'),
+      updated_at: new Date('2022-03-06T03:38:48.230Z'),
+    },
+  ];
+  const initialState = {
+    pokemons: {
+      pokemons,
+      loaded: true,
+      loading: false,
+      currPokemon: pokemons[0],
+    },
+  };
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [CreateEditFormComponent, SliderComponent],
+      imports: [
+        StoreModule.forRoot(appReducers),
+        HttpClientModule,
+        ReactiveFormsModule,
+      ],
+      providers: [
+        PokemonService,
+        provideMockStore<AppState>({
+          initialState,
+        }),
+        { provide: ComponentFixtureAutoDetect, useValue: true },
+      ],
+    });
+
     fixture = TestBed.createComponent(CreateEditFormComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    service = TestBed.inject(PokemonService);
+    store = TestBed.inject(MockStore);
+
+    component.ngOnInit();
   });
 
-  it('should create', () => {
+  it('debe crearse el formulario con los campos requeridos', () => {
     expect(component).toBeTruthy();
+    expect(component.registroForm.contains('name')).toBeTruthy();
+    expect(component.registroForm.contains('attack')).toBeTruthy();
+    expect(component.registroForm.contains('defense')).toBeTruthy();
+    expect(component.registroForm.contains('hp')).toBeTruthy();
+    expect(component.registroForm.contains('image')).toBeTruthy();
+    expect(component.registroForm.contains('type')).toBeTruthy();
+  });
+
+  it('debe inicializase el formulario con los datos de un pokemon', () => {
+    expect(component.registroForm.get('name')?.value).toBe(pokemons[0].name);
+    expect(component.registroForm.get('attack')?.value).toBe(
+      pokemons[0].attack
+    );
+    expect(component.registroForm.get('defense')?.value).toBe(
+      pokemons[0].defense
+    );
+    expect(component.registroForm.get('hp')?.value).toBe(pokemons[0].hp);
+    expect(component.registroForm.get('image')?.value).toBe(pokemons[0].image);
+    expect(component.registroForm.get('type')?.value).toBe(pokemons[0].type);
+  });
+
+  it('debe llamarse la función para editar cuando se de click en el botón con los datos correctos del formulario y cerrar el formulario', async () => {
+    fixture.whenStable().then(() => {
+      const alert = spyOn(window, 'alert');
+      const form = fixture.debugElement.query(By.css('form'));
+      const editService = spyOn(service, 'editPokemon').and.returnValue(
+        of(pokemons[0])
+      );
+
+      let closeFormFlag = false;
+      component.closeForm.subscribe((_) => (closeFormFlag = true));
+
+      form.triggerEventHandler('submit', null);
+
+      expect(editService).toHaveBeenCalledWith(
+        pokemons[0].id,
+        component.registroForm.value
+      );
+      expect(alert).toHaveBeenCalledWith('Pokemon editado correctamente');
+      expect(closeFormFlag).toBeTruthy();
+    });
+  });
+  it('debe llamarse la función para guardar nuevo Pokemon cuando se de click en el botón con los datos correctos del formulario y cerrar el formulario', async () => {
+    fixture.whenStable().then(() => {
+      //seteo el id de edit a -1 para que se tome como si fuera una creación
+      component.editID = -1;
+
+      const alert = spyOn(window, 'alert');
+      const form = fixture.debugElement.query(By.css('form'));
+      const saveService = spyOn(service, 'addPokemon').and.returnValue(
+        of(pokemons[0])
+      );
+
+      let closeFormFlag = false;
+      component.closeForm.subscribe((_) => (closeFormFlag = true));
+
+      form.triggerEventHandler('submit', null);
+
+      expect(saveService).toHaveBeenCalledWith({
+        ...component.registroForm.value,
+        idAuthor: 1,
+      });
+      expect(alert).toHaveBeenCalledWith('Pokemon creado correctamente');
+      expect(closeFormFlag).toBeTruthy();
+    });
+  });
+
+  it('debe llamarse al evento para cerrar el formulario al dar click en el botón de cancelar', async () => {
+    fixture.whenStable().then(() => {
+      const cancelBtn = fixture.debugElement.query(
+        By.css('button[type="button"]')
+      );
+
+      let closeFormFlag = false;
+      component.closeForm.subscribe((_) => (closeFormFlag = true));
+
+      cancelBtn.triggerEventHandler('click', null);
+
+      expect(closeFormFlag).toBeTruthy();
+    });
   });
 });
